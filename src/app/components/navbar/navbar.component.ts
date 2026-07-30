8ad0149bef4e34c771e83edc6709a4dc028f7113
Ronald Gutierrez Flores <deux.flores@gmail.com>
2026-07-30 08:44:52 -0400

    refactor: migrate navbar to primeng menubar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Productos',
+        icon: 'pi pi-box',
+        routerLink: '/productos'
+      },
+      {
+        label: 'Ventas',
+        icon: 'pi pi-shopping-cart',
+        routerLink: '/ventas'
+      }
+    ];
+  }
 }
